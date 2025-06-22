f5527ac3995e588ba11198162bbcb15951853dcd
bapesupreme <roundup.minibus_7f@icloud.com>
2025-06-22 14:58:09 -0700

    Create const.py

---
+++ custom_components/homgar/const.py
@@ -0,0 +1,10 @@
+"""Constants for the HomGar integration."""
+
+DOMAIN = "homgar"
+
+# Device types
+DEVICE_TYPE_HUB = "hub"
+DEVICE_TYPE_SOIL_MOISTURE = "soil_moisture"
+DEVICE_TYPE_RAIN_SENSOR = "rain_sensor"
+DEVICE_TYPE_AIR_SENSOR = "air_sensor"
+DEVICE_TYPE_WATER_TIMER = "water_timer"
